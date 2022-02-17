5415b316f4f2d92ab2ea33089e8e23cb7409f938
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-02-16 18:02:54 -0600

    add code comments

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// PASSWORD PROMPTS NEEDED
+
+// CHOOSE PASSWORD LENGTH BETWEEN 8 AND 128
+
+//Enter password length (between 8 and 128 characters)
+
+//Is passwordLength < 8 && > 128
+
+//You must choose a valid length
+
+// CHOOSE WHICH CHARACTER TYPES ARE ALLOWED
+
+//Please select at least one character type to include.
+//(repeat if null or blank)
+
+// ---lower case, upper case, numeric, special characters
+// ---must choose at least one type
+
+
