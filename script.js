57abbacb37cda002089bc55bef7159e3078504aa
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-02-18 19:48:22 -0600

    work on arrays

--- script.js
+++ script.js
@@ -20,18 +20,70 @@ generateBtn.addEventListener("click", writePassword);
 
 // CHOOSE PASSWORD LENGTH BETWEEN 8 AND 128
 
-//Enter password length (between 8 and 128 characters)
+var passwordLength = function () {
 
-//Is passwordLength < 8 && > 128
+  var promptLength = window.prompt('Please enter a password length between 8 - 128')
+  if (promptLength < 8 || promptLength > 128) { 
+      passwordLength = promptLength;
+  } else { 
 
+// if empty
 //You must choose a valid length
+//  alert "Please choose a password length between 8 and 128"
+window.alert ('Your password must be between 8-128 characters long. Please enter a password length between 8 - 128.');
+    return passwordLength();
 
-// CHOOSE WHICH CHARACTER TYPES ARE ALLOWED
+  }
+};
 
-//Please select at least one character type to include.
-//(repeat if null or blank)
+//CREATE MASTER ARRAY
 
-// ---lower case, upper case, numeric, special characters
-// ---must choose at least one type
+//  prompt "Please select at least one character type to include"
+
+var passwordCharacterArray = function() {
+  var promptCharacters = window.prompt('Please choose at least one character type for your password. Do you want to include upper case letters?')
+  //if yes, add upper case array to master array  master array + [uppercase]
+
+    window.prompt('Do you want to include lower case letters?')
+  //if yes, add lower case array to master array   master array + [lowercase]
+
+    window.prompt('Do you want to include numbers?')
+    //if yes. add number array to master array  master array + [numbers]
+
+    window.prompt('Do you want to include special characters?')
+    //if yes, add special characters to master array master array + [special]
+
+  //  if no character chosen, alert "Please choose at least one character type
+  if (promptCharacters === null) {
+    window.alert('You must choose at least one character type. Please choose at least one charactertype')
+  }
 
 
+//CHARACTER ARRRAYS
+
+
+var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'y', 'Z']
+
+var lowerCaseArray =
+
+var numeralArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+var specialCharacterArray = ['!', '@', '#', '$', '&', '*', '?']
+ 
+
+//CONCATENATE ARRAY TO MAKE MASTER ARRAY
+
+var masterCharacterArray = 
+
+
+//create password using Math.random * masterarraylength, i = password length
+
+
+};
+
+
+
+
+//for(var i+0; i < choosePassword length, i++) {
+          // ----add character to array
+//}
